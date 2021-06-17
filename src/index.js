364cb889c17f08b19a6b5ab529508f67b8bbb8db
Everton Santos <everton_santos_01@hotmail.com>
2021-06-17 16:37:52 -0300

    Componente com Filhos

--- src/index.js
+++ src/index.js
@@ -2,15 +2,9 @@ import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-// import Primeiro from './components/Primeiro'
-// import ComParametro from './components/ComParametro'
+import App from './App.jsx'
 
 ReactDOM.render(
-    <div>
-        {/* <Primeiro />
-        <ComParametro titulo="Esse é o título" 
-            subtitulo="Esse é o subtítulo" />
-        <ComParametro titulo="Opa" subtitulo="Epa" /> */}
-    </div>,
+    <App></App>,
     document.getElementById('root')
 )

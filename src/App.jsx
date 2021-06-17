8f02ba0ad3140935d4826ee604dd7f411404dc59
Everton Santos <everton_santos_01@hotmail.com>
2021-06-17 17:31:20 -0300

    Condicional: par ou ímpar

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,17 @@ import ComParametro from './components/ComParametro'
 import ComFilhos from './components/ComFilhos'
 import Card from './components/layout/Card'
 import Repeticao from './components/Repeticao'
+import Condicional from './components/Condicional'
+import CondicionalComIf from './components/CondicionalComif'
 
 export default (props) => (
     <div className="App">
+        <Card titulo="#06 - Condicional v2">
+            <CondicionalComIf numero={10}></CondicionalComIf>
+        </Card>
+        <Card titulo="#05 - Condicional v1">
+            <Condicional numero={11}></Condicional>
+        </Card>
         <Card titulo="#04 - Repetição">
             <Repeticao></Repeticao>
         </Card>
